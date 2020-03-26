2c146e01f8fca0a052610f642f6855da433adba5
Mariana Molinar <marianaml25@hotmail.com>
2020-03-26 09:46:29 -0600

    First commit

---
+++ holaMundo.js
@@ -0,0 +1,5 @@
+function saludar(nombre) {
+    return "Hola" + nombre;
+}
+var mi_nombre = "Mariana Molinar";
+var resultado = saludar(mi_nombre);
---
+++ holaMundo.ts
@@ -0,0 +1,7 @@
+function saludar (nombre){
+
+    return "Hola" + nombre
+}
+
+let mi_nombre="Mariana Molinar"
+let resultado = saludar(mi_nombre)
